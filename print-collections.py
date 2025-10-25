f2eb345ba67a4f0f316959264e705bdef8c76efe
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-25 19:52:21 +0200

    Add rest of required global attribute collections

--- print-collections.py
+++ print-collections.py
@@ -4,26 +4,50 @@ import esgvoc.api as ev
 
 
 def main():
+    fail_if_empty = True
+    fail_if_empty = False
+
     for collection in [
         "activity",
         "archive",
         "area_label",
         "branded_suffix",
+        "branded_variable",
         "contributor",
+        "contributor_member",
+        "conventions",
+        "creation_date",
+        "data_specs_version",
         "directory_date",
         "drs_specs",
         "experiment",
+        "forcing_index",
         "frequency",
         "grid",
+        "horizontal_label",
+        "initialization_index",
+        "license",
         "mip_era",
+        "nominal_resolution",
+        "physics_index",
+        "product",
+        "realization_index",
+        "realm",
         "region",
         "source",
+        "temporal_label",
         "time_range",
+        "tracking_id",
         "variable",
         "variant_label",
+        "vertical_label",
     ]:
         print(collection)
         ev_obj = ev.get_all_terms_in_collection("cmip7", collection)
+        if fail_if_empty and not ev_obj:
+            msg = f"{collection=} is empty"
+            raise ValueError(msg)
+
         for i, item in enumerate(ev_obj):
             for idx_f, field in enumerate(type(item).model_fields):
                 v = textwrap.wrap(
